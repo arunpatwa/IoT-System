5c5281cb9ce6071ab71f92dd19cea678bad7135f
Arun Patwa <91215615+arunpatwa@users.noreply.github.com>
2023-02-07 14:57:41 +0530

    Update Blink_LED.ino

--- Blink_LED.ino
+++ Blink_LED.ino
@@ -1,5 +1,7 @@
+ //THIS IS FOR BLINKING THE BUILT IN LED
 
-/* void setup() {
+ 
+ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
@@ -7,13 +9,13 @@
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
+  delay(2000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  delay(2000);                       // wait for a second
 }
-*/
 
-void setup() {
+
+/*void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   // HERE WE ARE CONNECTING LED ANODE TO THE 13 PIN  OF THE ARDUINO AND THE CATHODE TO THE GND
   pinMode(13, OUTPUT);
@@ -25,4 +27,4 @@ void loop() {
   delay(1000);                       // wait for a second
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
-}
+}*/
